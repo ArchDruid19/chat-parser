99c91db5e5c328f8b06358bbe9998ba017e6dc50
ArchDruid19 <druidvester@gmail.com>
2026-08-29 13:40:31 -0400

    More GUI improvements

--- chat_parser/qt6_gui/main_window.py
+++ chat_parser/qt6_gui/main_window.py
@@ -1,7 +1,10 @@
 from chat_parser.chat.yt_chat import YTChat
 import chat_parser.chat.yt_chat_parser as yt
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (
     QFileDialog,
+    QHBoxLayout,
+    QLineEdit,
     QMainWindow,
     QPushButton,
     QVBoxLayout,
@@ -18,33 +21,47 @@ class MainWindow(QMainWindow):
 
         # Widgets
         self.lst_chat: QListWidget = QListWidget()
-        self.btn_open_chat: QPushButton = QPushButton("Open File")
 
-        # Event handlers
-        self.btn_open_chat.clicked.connect(self.open_json_file)
+        # Top menu bar (from QMainWindow)
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu("File")
+        file_open_action = QAction("Open", self)
+        file_menu.addAction(file_open_action)
+
+        # Search layout
+        self.btn_search_user: QPushButton = QPushButton("Search")
+        self.txt_search_user: QLineEdit = QLineEdit()
+
+        search_layout_hbox = QHBoxLayout()
+        search_layout_hbox.addWidget(self.btn_search_user)
+        search_layout_hbox.addWidget(self.txt_search_user)
+
+        search_layout_widget = QWidget()
+        search_layout_widget.setLayout(search_layout_hbox)
 
         # Main layout
         main_layout: QVBoxLayout = QVBoxLayout()
         main_layout.addWidget(self.lst_chat)
-        main_layout.addWidget(self.btn_open_chat)
+        main_layout.addWidget(search_layout_widget)
 
         main_widget: QWidget = QWidget()
         main_widget.setLayout(main_layout)
 
         # Main window
         self.setWindowTitle("Chat Viewer")
-        self.setFixedSize(800, 600)
+        self.setFixedSize(600, 400)
         self.setCentralWidget(main_widget)
+
+        # Event handlers
+        self.btn_search_user.clicked.connect(self.search_user)
+        file_open_action.triggered.connect(self.open_json_file)
 
     def open_json_file(self) -> str:
         file_path: tuple = QFileDialog.getOpenFileName(
-            self,
-            "Open Chat",
-            "",
-            "JSON Files (*.json);;All Files (*)"
+            self, "Open Chat", "", "JSON Files (*.json);;All Files (*)"
         )
 
-        if file_path:
+        if file_path[0]:
             self.yt_chat = yt.json_to_yt_chat([file_path[0]])
             self.fill_lst_chat()
 
@@ -55,3 +72,7 @@ class MainWindow(QMainWindow):
             for msg in self.yt_chat.chat[username].messages:
                 self.lst_chat.addItem(msg.get_msg())
             self.lst_chat.addItem("\n")
+
+    def search_user(self):
+        print(self.lst_chat.item(0))
+        pass
